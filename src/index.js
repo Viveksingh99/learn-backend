3bbbd061eadb8231bf6a6333542a18e06ed64d86
Viveksingh99 <vivek22jan99@gmail.com>
2024-02-24 15:25:45 +0530

    db connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+dotenv.config({
+    path: './env'
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async ()=>{
+  try {
+    await Mongoose.connect (`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error",(error)=>{
+      console.log("ERRR:", error)
+      throw error
+    })
+    app.listen(process.env.PORT, () => {
+      console.log(`App port ${process.env.PORT}`)
+    })
+  } catch(error){
+    console.error("ERROR: ", error)
+    throw error
+  }
 })
+*/
